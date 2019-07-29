db2908616535090f880129991b118a32d335beaf
Akash Rathore <akash.rathore@continuum.net>
2019-07-29 16:38:43 +0530

    AkashCommit

--- src/test/java/com/Cont/test/TestCases1.java
+++ src/test/java/com/Cont/test/TestCases1.java
@@ -1,5 +1,7 @@
 package com.Cont.test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -13,6 +15,8 @@ public void userNameVerification() {
 	test.log(Status.INFO, "Inside first test case");
 	test.log(Status.INFO, "Starting test case");
 	test.log(Status.INFO, "Before asserting");
+	WebDriver driver= new FirefoxDriver();
+	driver.get("www.gmail.com");
 	Assert.assertEquals("Akash1", "Akash1", "Failed to verify users first name");
 }
 }
