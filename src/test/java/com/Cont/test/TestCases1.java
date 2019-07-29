81339bda81a6148c2706de68a9d34916e550dbd9
Akash Rathore <akash.rathore@continuum.net>
2019-07-29 17:10:41 +0530

    cvbgdbdbd

--- src/test/java/com/Cont/test/TestCases1.java
+++ src/test/java/com/Cont/test/TestCases1.java
@@ -1,7 +1,5 @@
 package com.Cont.test;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -15,10 +13,6 @@ public void userNameVerification() {
 	test.log(Status.INFO, "Inside first test case");
 	test.log(Status.INFO, "Starting test case");
 	test.log(Status.INFO, "Before asserting");
-	System.out.println("Driver path in jenkins machine is");
-	System.out.println(System.getProperty("user.dir")+"//drivers//chromedriver.exe");
-	System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"//drivers//chromedriver.exe");
-	WebDriver driver= new ChromeDriver();
 	Assert.assertEquals("Akash1", "Akash1", "Failed to verify users first name");
 }
 }
