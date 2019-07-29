bef1b03c0f380b086f990567ddb454d5cd83efcd
Akash Rathore <akash.rathore@continuum.net>
2019-07-29 11:34:16 +0530

    AkashCommit

--- src/main/java/com/base/utils/BaseTest.java
+++ src/main/java/com/base/utils/BaseTest.java
@@ -9,11 +9,13 @@ import org.testng.annotations.BeforeTest;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.ChartLocation;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class BaseTest  {
-	public ExtentReports extent;
-	 public ExtentHtmlReporter htmlReporter;
-	 public ExtentTest test;
+	public static ExtentReports extent;
+	 public static ExtentHtmlReporter htmlReporter;
+	 public static ExtentTest test;
 	
 	@BeforeTest
 	public void setup() {
@@ -23,18 +25,20 @@ public class BaseTest  {
         //initialize ExtentReports and attach the HtmlReporter
         extent = new ExtentReports();
         extent.attachReporter(htmlReporter);
+        
+      //To add system or environment info by using the setSystemInfo method.
+        extent.setSystemInfo("OS", "Winsowd");
+        extent.setSystemInfo("Browser", "Chrome");
+        
+        //configuration items to change the look and feel
+        //add content, manage tests etc
+        htmlReporter.config().setChartVisibilityOnOpen(true);
+        htmlReporter.config().setDocumentTitle("Extent Report Demo");
+        htmlReporter.config().setReportName("Test Report");
+        htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
+        htmlReporter.config().setTheme(Theme.STANDARD);
+        htmlReporter.config().setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a '('zzz')'");
 		
-		/*
-		 * //To add system or environment info by using the setSystemInfo method.
-		 * extent.setSystemInfo("OS", "Windows"); extent.setSystemInfo("Browser",
-		 * "Chrome"); htmlReporter.config().setChartVisibilityOnOpen(true);
-		 * htmlReporter.config().setDocumentTitle("Extent Report Demo");
-		 * htmlReporter.config().setReportName("Test Report");
-		 * htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
-		 * htmlReporter.config().setTheme(Theme.STANDARD); htmlReporter.config().
-		 * setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a '('zzz')'");
-		 */
-       
 		
 	}
 	 @BeforeMethod
