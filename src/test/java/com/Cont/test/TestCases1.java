cbb10df1400146545788db4a2907ef4a9753622a
Akash Rathore <akash.rathore@continuum.net>
2019-07-29 16:46:26 +0530

    AkashCommit

--- src/test/java/com/Cont/test/TestCases1.java
+++ src/test/java/com/Cont/test/TestCases1.java
@@ -1,7 +1,7 @@
 package com.Cont.test;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -15,8 +15,8 @@ public void userNameVerification() {
 	test.log(Status.INFO, "Inside first test case");
 	test.log(Status.INFO, "Starting test case");
 	test.log(Status.INFO, "Before asserting");
-	WebDriver driver= new FirefoxDriver();
-	driver.get("www.gmail.com");
+	System.setProperty("webdriver.chrome.driver","C://Users//akash.rathore//.m2//repository//webdriver//chromedriver//win32//76.0.3809.25//chromedriver.exe");
+	WebDriver driver= new ChromeDriver();
 	Assert.assertEquals("Akash1", "Akash1", "Failed to verify users first name");
 }
 }
