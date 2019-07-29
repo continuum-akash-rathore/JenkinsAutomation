c6fc43c9996e9ba49aa7216cedaad8fe5359985d
Akash Rathore <akash.rathore@continuum.net>
2019-07-29 14:48:59 +0530

    AkashCommit

--- src/test/java/com/Cont/test/TestCases1.java
+++ src/test/java/com/Cont/test/TestCases1.java
@@ -13,6 +13,6 @@ public void userNameVerification() {
 	test.log(Status.INFO, "Inside first test case");
 	test.log(Status.INFO, "Starting test case");
 	test.log(Status.INFO, "Before asserting");
-	Assert.assertEquals("Akash", "Akash", "Failed to verify users first name");
+	Assert.assertEquals("Akash1", "Akash1", "Failed to verify users first name");
 }
 }
